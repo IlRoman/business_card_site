94d7313d1147d71354f155448a16436c3f5c7246
il_roman <il.roman@ukr.net>
2023-06-16 16:02:15 +0300

    page3 text with translation added

--- src/pages/Home/Page3/Page3.jsx
+++ src/pages/Home/Page3/Page3.jsx
@@ -1,9 +1,12 @@
+import { useTranslation } from 'react-i18next';
 import './main.scss';
 
 export const Page3 = () => {
+    const { t } = useTranslation();
+
     return (
         <div className='page3'>
-            <h1 className='title'>Main</h1>
+            <h1 className='title'>{`${t("Do not judge me strictly for the design, I'm not a designer")} =)`}</h1>
         </div>
     )
 };
